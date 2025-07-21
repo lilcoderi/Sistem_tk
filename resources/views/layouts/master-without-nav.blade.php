880b055f229092ef46d06065afc65b62782ed2b5
Little Coder <kimriana@yahoo.com>
2025-07-22 02:18:44 +0700

    Update master-without-nav.blade.php

--- resources/views/layouts/master-without-nav.blade.php
+++ resources/views/layouts/master-without-nav.blade.php
@@ -3,35 +3,21 @@
 
 <head>
     <meta charset="utf-8" />
-    <title>@yield('title', 'Dashboard') | TK MARHAMAH HASANAH 2</title>
+    <title>@yield('title') | TK MARHAMAH HASANAH 2</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Dashboard TK Marhamah Hasanah 2">
-    <meta name="author" content="Hartono">
-    
-    <!-- App Favicon -->
+    <meta content="Halaman login TK Marhamah Hasanah 2" name="description" />
+    <meta content="Hartono" name="author" />
     <link rel="shortcut icon" href="{{ secure_asset('build/images/favicon.ico') }}">
-
-    <!-- Include Head CSS -->
+    
+    {{-- Include CSS --}}
     @include('layouts.head-css')
-
-    {{-- Page-specific styles --}}
-    @yield('css')
 </head>
 
 <body>
-
-    {{-- Page Content --}}
     @yield('content')
 
-    {{-- Optional Right Sidebar --}}
-    {{-- @include('layouts.right-sidebar') --}}
-
-    {{-- Vendor Scripts --}}
+    {{-- Include JS --}}
     @include('layouts.vendor-scripts')
-
-    {{-- Page-specific scripts --}}
-    @yield('scripts')
-    
 </body>
 
 </html>
