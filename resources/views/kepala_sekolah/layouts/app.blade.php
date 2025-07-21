a12c2ad9309ba59e98f2e392df8b4459f39f61cd
Little Coder <kimriana@yahoo.com>
2025-07-22 01:40:40 +0700

    Update app.blade.php

--- resources/views/kepala_sekolah/layouts/app.blade.php
+++ resources/views/kepala_sekolah/layouts/app.blade.php
@@ -17,6 +17,9 @@
     <!-- Tambahkan ini di bagian head jika belum ada -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
 
+    <!-- Bootstrap CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
 
     {{-- Icons & Styles --}}
     <!-- Bootstrap Icons CSS -->
