bb02d4a2702cef66bfe7f52c43adcdbce5c96931
Little Coder <kimriana@yahoo.com>
2025-07-22 02:34:07 +0700

    Update head-css.blade.php

--- resources/views/layouts/head-css.blade.php
+++ resources/views/layouts/head-css.blade.php
@@ -3,6 +3,9 @@
 <link href="{{ secure_asset('build/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
 <link href="{{ secure_asset('build/css/app.min.css') }}" rel="stylesheet" type="text/css" />
 
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
 {{-- Tambahan opsional layout JS seperti pengaturan tema --}}
 <script src="{{ secure_asset('build/js/pages/layout.js') }}"></script>
 
